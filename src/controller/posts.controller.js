d6cf2ec84589d6fb09ced6bfdf317900d9e0a868
NikitaParasToliya <nikita-paras.toliya@hpe.com>
2024-07-10 10:33:58 +0530

    Reordering folder structure

---
+++ src/controller/posts.controller.js
@@ -0,0 +1,15 @@
+ export async function apiCreatePost(req, res) {
+   try {
+     const loggedInUser = req.user;
+     const result = await createNewProduct(req.body, loggedInUser);
+     res.status(result.status).json({
+       success: result.success,
+       data: result.data || null,
+       message: result.message,
+     });
+   } catch (err) {
+     res
+       .status(500)
+       .json({ success: false, message: err.message || 'Internal server err' });
+   }
+ }
