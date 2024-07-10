1d74ec1a42ffd38d7ea43c4894a48e9186f087c0
Dinoop <dinoop.p@hpe.com>
2024-07-10 12:32:09 +0530

    docker files initial commit

---
+++ build-image.bat
@@ -0,0 +1,11 @@
+set IMAGE_LIBRARY=dinooppaloli
+set IMAGE_NAME=posts
+set IMAGE_TAG=1.0
+set IMAGE_VERSIONED=%IMAGE_LIBRARY%/%IMAGE_NAME%:%IMAGE_TAG%
+set IMAGE_LATEST=%IMAGE_LIBRARY%/%IMAGE_NAME%:latest
+
+docker image build -t %IMAGE_VERSIONED% .
+docker image tag %IMAGE_VERSIONED% %IMAGE_LATEST%
+
+docker push %IMAGE_VERSIONED%
+docker push %IMAGE_LATEST%
