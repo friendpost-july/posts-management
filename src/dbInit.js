eab144c7a0032dfa90f7ab6309fc3fe8c50461f0
nikhilamy <nikhilamy@hpe.com>
2024-07-10 11:47:13 +0530

    env changes

--- src/dbInit.js
+++ src/dbInit.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const mongo_url = process.env.MONGO_URL;
 
 async function dbInit() {
   console.log('Connecting to DB....');
   try {
-    mongoose.connect('mongodb://127.0.0.1:37017');
+    mongoose.connect(mongo_url);
     console.log('Database connected');
   } catch (error) {
     console.log('Error in connecting to the database');
