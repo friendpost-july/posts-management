09518436242fe04a8da5204b3a960c30233b09ab
NikitaParasToliya <nikita-paras.toliya@hpe.com>
2024-07-10 16:33:54 +0530

    Added counter for create posts

--- src/helper/common.js
+++ src/helper/common.js
@@ -1,12 +1,21 @@
 import { counterModal } from '../schemas/counter.js';
 
 export const getPostID = async () => {
-    const postCounter = await counterModal.findOneAndUpdate({}, { $inc: { postId: 1 } }, { new: true }).lean();
-    console.log(postCounter);
-    return postCounter.postId;
+  const postCounter = await counterModal
+    .findOneAndUpdate({}, { $inc: { postId: 1 } }, { new: true })
+    .lean();
+  console.log(postCounter);
+  return postCounter.postId;
 };
 
 export const createCounterCollection = async () => {
-    await counterModal.findOneAndUpdate({ postId: 0 }, { upsert: 1 });
+  const isCollectionExist = await counterModal.find({});
+  if (!isCollectionExist.length) {
+    await counterModal.findOneAndUpdate(
+      {},
+      { $set: { postId: 0 } },
+      { upsert: 1 }
+    );
     console.log('successfully inserted document');
+  }
 };
--- src/methods/posts.method.js
+++ src/methods/posts.method.js
@@ -3,15 +3,19 @@ import { getPostID } from '../helper/common.js';
 
 export async function createNewPost(userId, text, visibility) {
   try {
+    console.log(userId, text, visibility);
     //Check if input is valid
     if (!userId || !text || !visibility) {
+      console.log(userId);
       throw new Error('userId, text, or visibility is missing or undefined');
     }
     let counter = await getPostID();
+    console.log('Creating new posts');
     await postModal.create({ userId, text, visibility, postId: counter });
+    console.log('Created new posts successfully');
     return {
       status: 200,
-      message: { postId: counter, visibility: visibility }
+      message: { postId: counter, visibility: visibility },
     };
   } catch (error) {
     return { status: 400, success: false, message: error };
