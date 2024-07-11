189239cfed7897425597236a15de939adc3bd65e
Chetan S Vakkalad <chetan-s.vakkalad@hpe.com>
2024-07-11 16:50:24 +0530

    fix: added date time in the schema

--- src/schemas/posts.js
+++ src/schemas/posts.js
@@ -2,16 +2,17 @@ import pkg from 'mongoose';
 const { Schema, model } = pkg;
 
 export const postModal = model(
-  'posts',
-  new Schema(
-    {
-      userId: { type: String, required: true },
-      postId: { type: Number },
-      text: { type: String, require: true },
-      visibility: { type: String, required: true, enum: ['private', 'public'] }
-    },
-    {
-      collection: 'posts'
-    }
-  )
+    'posts',
+    new Schema(
+        {
+            userId: { type: String, required: true },
+            postId: { type: Number },
+            text: { type: String, require: true },
+            visibility: { type: String, required: true, enum: ['private', 'public'] },
+            createdAt: { type: Date, default: Date.now },
+        },
+        {
+            collection: 'posts',
+        },
+    ),
 );
