8f5eeb3917ca769173a46402832935aeb2a111d3
NikitaParasToliya <nikita-paras.toliya@hpe.com>
2024-07-11 15:03:32 +0530

    Adding searchAllPosts

--- src/routes/posts.search.route.js
+++ src/routes/posts.search.route.js
@@ -2,6 +2,6 @@ import { Router } from 'express';
 import { apiGetAllPosts } from '../controller/posts.controller.js';
 
 export const searchRouter = new Router();
-router.route('/').post(apiGetAllPosts);
+searchRouter.route('/').post(apiGetAllPosts);
 
 
