27e34fad74275fa226669b3f817b741d958d2f1b
Raj Chaudhuri <rajch@hotmail.com>
2024-07-12 02:10:14 +0530

    Added PostAdded domain event through FriendPostPostAdded RabbitMQ exchange

--- src/methods/posts.method.js
+++ src/methods/posts.method.js
@@ -1,5 +1,23 @@
 import { postModal } from '../schemas/posts.js';
 import { getPostID } from '../helper/common.js';
+import amqp from 'amqplib';
+
+const rabbitMqUrl = process.env.RABBITMQ_URL || 'amqp://user:password@localhost'; // Replace with your RabbitMQ server URL
+
+async function postToRmqExchange(exchangeName, messagedata, operation) {
+  try {
+    const connection = await amqp.connect(rabbitMqUrl);
+    const channel = await connection.createChannel();
+
+    await channel.assertExchange(exchangeName, 'fanout', { durable: false });
+    // Send data like this:
+    channel.publish(exchangeName, '', Buffer.from(JSON.stringify({ ...messagedata, timestamp: new Date(), operation: operation })));
+
+    console.log(`${exchangeName} sent data to RabbitMQ:`, { ...messagedata, timestamp: new Date(), operation: operation });
+  } catch (error) {
+    console.error(`Error connecting ${exchangeName} to RabbitMQ:`, error);
+  }
+}
 
 export async function createNewPost(userId, text, visibility) {
   try {
@@ -13,6 +31,8 @@ export async function createNewPost(userId, text, visibility) {
     console.log('Creating new posts');
     await postModal.create({ userId, text, visibility, postId: counter });
     console.log('Created new posts successfully');
+    console.log('Sending to event bus');
+    await postToRmqExchange('FriendPostPostAdded', { post: { userId, text, visibility, postId: counter } });
     return {
       status: 200,
       data: { postId: counter, visibility: visibility },
@@ -96,10 +116,10 @@ export async function getAllPosts(
 
 
 export const deletPostsByuserID = async (userId) => {
-    const userDeleted = await postModal.deleteMany({ userId });
-    console.log(userDeleted);
-    return {
-        status: userDeleted.deletedCount ? 200 : 404,
-        message: !userDeleted.deletedCount && 'Post does not exits for the user ID',
-    };
+  const userDeleted = await postModal.deleteMany({ userId });
+  console.log(userDeleted);
+  return {
+    status: userDeleted.deletedCount ? 200 : 404,
+    message: !userDeleted.deletedCount && 'Post does not exits for the user ID',
+  };
 };
